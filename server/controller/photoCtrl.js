8dd132db130eb5eeac650958d48f66ef8c1b7fce
is2mael <ismael.muh26@gmail.com>
2024-10-30 21:42:10 +0700

    Feat: Add Cloudinary

---
+++ server/controller/photoCtrl.js
@@ -0,0 +1,12 @@
+exports.postFavorite = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        console.log("🚀 ~ exports.postFavorite= ~ err:", err)
+        
+    }
+}
+
+
+exports.deleteFavorite
+exports.listFavorite
--- server/controller/userCtrl.js
+++ server/controller/userCtrl.js
@@ -1,83 +1,86 @@
-const { comparePassword } = require("../helpers/bcrypt")
-const { generateToken } = require("../helpers/jwt")
-const { User } = require("../models")
+require('dotenv').config()
+const { comparePassword } = require("../helpers/bcrypt");
+const { generateToken } = require("../helpers/jwt");
+const { User } = require("../models");
+const upload = require("../helpers/cloudynary")
 
-exports.register = async(req, res, next) => {
-    const { username, email, password, imgUrl } = req.body
-    try {
-        const newUser = await User.create({
-            username,
-            email,
-            password, 
-            imgUrl
-        })
-        res.status(201).json({
-            id: newUser.id,
-            username: newUser.username,
-            email: newUser.email
-        })
-    } catch (err) {
-        console.log("🚀 ~ exports.register=async ~ error:", error)
-        next(err)       
+exports.register = async (req, res, next) => {
+  const { username, email, password, imgUrl } = req.body;
+  try {
+    const newUser = await User.create({
+      username,
+      email,
+      password,
+      imgUrl,
+    });
+    res.status(201).json({
+      id: newUser.id,
+      username: newUser.username,
+      email: newUser.email,
+    });
+  } catch (err) {
+    console.log("🚀 ~ exports.register=async ~ error:", error);
+    next(err);
+  }
+};
+
+exports.Login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    if (!email) {
+      throw { name: "BadRequest", message: "Email is required" };
     }
-}
-
-exports.Login = async(req, res, next) => {
-    const { email, password } = req.body
-    try {
-        if (!email) {
-            throw { name: "BadRequest", message: "Email is required" }
-        }
-        if (!password) {
-            throw { name: "BadRequest", message: "Password is required" }
-        }
-
-        const newUser = await User.findOne({
-            where: {
-                email
-            }
-        })
-
-        if (!newUser || !comparePassword(password, newUser.password)) {
-            throw { name: "Unauthorized", message: "Invalid email/password" }
-        }
-
-        let access_token = generateToken(newUser)
-        res.status(200).json({ access_token })
-    } catch (err) {
-        console.log("🚀 ~ exports.Login=async ~ err:", err)
-        next(err)
+    if (!password) {
+      throw { name: "BadRequest", message: "Password is required" };
     }
-}
+
+    const newUser = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!newUser || !comparePassword(password, newUser.password)) {
+      throw { name: "Unauthorized", message: "Invalid email/password" };
+    }
+
+    let access_token = generateToken(newUser);
+    res.status(200).json({ access_token });
+  } catch (err) {
+    console.log("🚀 ~ exports.Login=async ~ err:", err);
+    next(err);
+  }
+};
 
 exports.UserById = async (req, res, next) => {
-    try {
-        const user = await User.findOne({
-            where: { id: req.user.id },
-            attributes: ["id", "name", "imgUrl"]
-        })
-        res.status(200).json(user)
-    } catch (err) {
-        console.log("🚀 ~ exports.UserById= ~ err:", err)
-        next(err)
-    }
-}
+  try {
+    const user = await User.findOne({
+      where: { id: req.user.id },
+      attributes: ["id", "name", "imgUrl"],
+    });
+    res.status(200).json(user);
+  } catch (err) {
+    console.log("🚀 ~ exports.UserById= ~ err:", err);
+    next(err);
+  }
+};
 
 exports.UpdateUser = async (req, res, next) => {
-    const { username, imgUrl } = req.body
-    try {
-        await User.update({
-            username,
-            imgUrl
-        },
-        {
-            where: {
-                id: req.user.id
-            }
-        })
-        res.status(200).json({ message: "Update success" })
-    } catch (err) {
-        console.log("🚀 ~ exports.UpdateUser= ~ err:", err)
-        next(err)
+  const userId = req.user.id;
+  try {
+    let user = await User.findByPk(userId);
+    if (!user) {
+      throw { name: "Not Found", message: "User not found" };
     }
-}
+
+    const uploadResult = await upload(req.file, "User", user, "Random");
+    user.imgUrl = uploadResult.secure_url;
+
+    // Save the updated user data
+    await user.save();
+    res.status(200).json({ message: `Profile image updated successfully`, user });
+  } catch (err) {
+    console.log("🚀 ~ exports.UpdateUser= ~ err:", err);
+    next(err);
+  }
+};
