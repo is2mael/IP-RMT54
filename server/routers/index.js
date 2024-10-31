0197866522b3f43aa4627f2cfaf2e18506ec454a
is2mael <ismael.muh26@gmail.com>
2024-10-31 11:21:24 +0700

    Feat: multer and cloudinary now works

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,7 +1,11 @@
+const { postFavorite, deleteFav, listFav } = require("../controller/favoriteCtrl")
 const { register, Login, UserById, updateUser } = require("../controller/userCtrl")
 const authentication = require("../middlewares/autentication")
 const authorization = require("../middlewares/authorization")
 const errorHandler = require("../middlewares/errorhandler")
+const multer  = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 const router = require("express").Router()
 
@@ -14,7 +18,11 @@ router.post("/login", Login)
 
 router.use(authentication)
 
-router.patch("/user/:id/image", updateUser)
+router.patch("/user/:id/imgUrl", upload.single("file"), updateUser)
+router.post("/favorite", postFavorite)
+router.delete("/favorite", deleteFav)
+router.get("/favotite", listFav)
+
 router.use(errorHandler)
 
 module.exports = router
