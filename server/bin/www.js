139245d75e33a662e2a2403cb5f03baa9579b7d8
is2mael <ismael.muh26@gmail.com>
2026-04-08 21:15:54 +0700

    recoll server

--- server/bin/www.js
+++ server/bin/www.js
@@ -2,7 +2,8 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const app = require("../app")
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
